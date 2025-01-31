5f29a522b6b889fe2e4d6293bbe7bfc2599edf2b
Maksym Boichuk <max.turbo.star21@gmail.com>
2025-01-31 12:37:27 +0000

    weaksa

---
+++ weaksa/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.WriteLine("write number");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("monday");
+}
+else if (num == 2)
+{
+    Console.WriteLine("tuesday");
+}
+else if (num == 3)
+{
+    Console.WriteLine("wednesday");
+}
+else if (num == 4)
+{
+    Console.WriteLine("thursday");
+}
+else if (num == 5)
+{
+    Console.WriteLine("friday");
+}
+else if (num == 6)
+{
+    Console.WriteLine("saturday");
+}
+else if (num == 7)
+{
+    Console.WriteLine("sunday");
+}
